23862c00bade3bc4fe9a8c6d8333f5bd3a03be93
nurislammridha <mridha851@gmail.com>
2022-01-17 15:28:32 +0600

    U[date

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const app = express();
-//Connect Database
+//Connect Database//git
 connectDB();
 //Initial middleware
 app.use(express.json({ extended: false }));
